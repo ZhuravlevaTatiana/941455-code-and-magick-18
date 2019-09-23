d209157747020d952dde8238293c4271f2cefdb1
ZhuravlevaTatiana <guravleva@gro-rm.ru>
2019-09-23 15:46:43 +0300

    проверк2

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,3 @@
-
-
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -9,7 +7,6 @@ var FONT_GAP = 15;
 var COLUMN_WIDTH = 40;
 var COLUMN_HEIGHT = 150;
 var COLUMN_GAP = 50;
-
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -17,7 +14,6 @@ var renderCloud = function (ctx, x, y, color) {
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP);
@@ -32,22 +28,19 @@ window.renderStatistics = function (ctx, names, times) {
     return maxElement;
   };
   var maxTime = getMaxElement(times);
-
   for (var i = 0; i < names.length; i++) {
     var currentTime = times[i];
     var currentPlayer = names[i];
     var columnHeight = currentTime / maxTime * COLUMN_HEIGHT;
     var columnX = CLOUD_X + GAP * 2 + COLUMN_WIDTH * 2.5 * i;
-
     ctx.fillStyle = '#000';
-    ctx.fillText(currentPlayer, columnX, y: COLUMN_GAP + FONT_GAP);
-    ctx.fillText(Math.round(currentTime), columnX, y: COLUMN_GAP - columnHeight - GAP);
-
+    ctx.fillText(currentPlayer, columnX, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(currentTime), columnX, CLOUD_HEIGHT - columnHeight - GAP);
     if (currentPlayer === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'hsl(250, ' + Math.random() * 100 + '%, 50%)';
     }
-    ctx.fillRect(columnX, y: COLUMN_GAP - columnHeight, COLUMN_WIDTH, columnHeight);
+    ctx.fillRect(columnX, CLOUD_HEIGHT - columnHeight - GAP, COLUMN_WIDTH, columnHeight);
   }
 };
