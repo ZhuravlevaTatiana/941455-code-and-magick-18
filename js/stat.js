98e96d7b7ef2e9eea08a4518c40a339cc24f901f
ZhuravlevaTatiana <guravleva@gro-rm.ru>
2019-09-23 14:52:49 +0300

    проверить

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -7,11 +9,11 @@ var FONT_GAP = 15;
 var COLUMN_WIDTH = 40;
 var COLUMN_HEIGHT = 150;
 var COLUMN_GAP = 50;
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -20,11 +22,32 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP * 2);
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+    return maxElement;
+  };
+  var maxTime = getMaxElement(times);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Вы', CLOUD_X + COLUMN_GAP, CLOUD_Y + 260, COLUMN_WIDTH);
-  ctx.fillRect(CLOUD_X + COLUMN_GAP, 100, COLUMN_WIDTH, COLUMN_HEIGHT);
+  /*for (var i = 0; i < names.length; i++) {
+    var currentTime = times[i];
+    var currentPlayer = names[i];
+    var columnHeight = currentTime / maxTime * COLUMN_HEIGHT;
+    var columnX = CLOUD_X + GAP * 2 + COLUMN_WIDTH * 2.5 * i;
 
+    ctx.fillStyle = '#000';
+    ctx.fillText(currentPlayer, columnX, y: COLUMN_GAP + FONT_GAP);
+    ctx.fillText(Math.round(currentTime), columnX, y: COLUMN_GAP - columnHeight - GAP);
 
+    if (currentPlayer === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(250, ' + Math.random() * 100 + '%, 50%)';
+    }
+    ctx.fillRect(columnX, y: COLUMN_GAP - columnHeight, COLUMN_WIDTH, columnHeight);
+  }*/
 };
